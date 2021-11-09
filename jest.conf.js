d317193529f87e1e9318db9467d72c2bafb9b598
Huy Duc Dao <ducdh.starvn@gmail.com>
2021-11-10 00:22:05 +0700

    First code commit

---
+++ jest.conf.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2021 Huy Duc Dao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const tsconfig = require('./tsconfig.json');
+
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testURL: 'http://localhost/',
+  cacheDirectory: '<rootDir>/build/jest-cache',
+  coverageDirectory: '<rootDir>/build/test-results/',
+  testMatch: ['<rootDir>/src/**/@(*.)@(spec.ts?(x))'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: mapTypescriptAliasToJestAlias({
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  }),
+  reporters: ['default', ['jest-junit', { outputDirectory: './build/test-results/', outputName: 'TESTS-results-jest.xml' }]],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.test.json',
+      compiler: 'typescript',
+      diagnostics: false,
+    },
+  },
+};
+
+function mapTypescriptAliasToJestAlias(alias = {}) {
+  const jestAliases = { ...alias };
+  if (!tsconfig.compilerOptions.paths) {
+    return jestAliases;
+  }
+  Object.entries(tsconfig.compilerOptions.paths)
+    .filter(([key, value]) => {
+      // use Typescript alias in Jest only if this has value
+      if (value.length) {
+        return true;
+      }
+      return false;
+    })
+    .map(([key, value]) => {
+      // if Typescript alias ends with /* then in Jest:
+      // - alias key must end with /(.*)
+      // - alias value must end with /$1
+      const regexToReplace = /(.*)\/\*$/;
+      const aliasKey = key.replace(regexToReplace, '$1/(.*)');
+      const aliasValue = value[0].replace(regexToReplace, '$1/$$1');
+      return [aliasKey, `<rootDir>/${aliasValue}`];
+    })
+    .reduce((aliases, [key, value]) => {
+      aliases[key] = value;
+      return aliases;
+    }, jestAliases);
+  return jestAliases;
+}
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 Huy Duc Dao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const webpack = require('webpack');
+
+const path = require('path');
+
+const ROOT = path.resolve(__dirname, '.');
+
+const TEST = process.argv.indexOf('--test') > -1;
+
+function root(args) {
+  return path.join.apply(path, [ROOT].concat(args));
+}
+
+module.exports = {
+  devtool: 'inline-source-map',
+  mode: 'production',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    modules: ['node_modules'],
+  },
+
+  entry: root('xreact-core-libs.ts'),
+
+  output: {
+    path: root('bundles'),
+    publicPath: '/',
+    filename: 'xreact-core-libs.umd.js',
+    libraryTarget: 'umd',
+    library: 'xreact-core-libs',
+  },
+
+  optimization: {
+    moduleIds: 'named',
+  },
+
+  // require those dependencies but don't bundle them
+
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(j|t)sx?$/,
+        loader: 'eslint-loader',
+        exclude: [root('node_modules')],
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: [/\.e2e\.ts$/],
+      },
+    ],
+  },
+
+  externals: TEST
+    ? {
+        cheerio: 'window',
+        'react/addons': true,
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true,
+      }
+    : {
+        lodash: true,
+        react: true,
+        'react-addons-css-transition-group': true,
+        'react-addons-transition-group': true,
+        'react-dom': true,
+      },
+};
