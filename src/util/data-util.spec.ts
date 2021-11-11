7c8055dd569bb168605b82a31b83c6fbb5b1e30d
Huy Duc Dao <ducdh.starvn@gmail.com>
2021-11-11 23:41:25 +0700

    Remove license header file

--- src/util/data-util.spec.ts
+++ src/util/data-util.spec.ts
@@ -1,36 +1,19 @@
-''/*
- * Copyright (c) 2021 Huy Duc Dao
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import { size, byteSize } from './index';
 
-import { byteSize, size } from "./index";
-
-describe("Data utils", () => {
-  describe("size", () => {
-    it("should return the correct size", () => {
-      const data = "Hello StarVN";
+describe('Data utils', () => {
+  describe('size', () => {
+    it('should return the correct size', () => {
+      const data = 'Hello Jhipster';
       expect(size(data)).toBe(10.5);
-      expect(size("")).toBe(0);
+      expect(size('')).toBe(0);
     });
   });
 
-  describe("byteSize", () => {
-    it("should return the correct value", () => {
-      const data = "Hello StarVN";
-      expect(byteSize(data)).toBe("10.5 bytes");
-      expect(byteSize("")).toBe("0 bytes");
+  describe('byteSize', () => {
+    it('should return the correct value', () => {
+      const data = 'Hello Jhipster';
+      expect(byteSize(data)).toBe('10.5 bytes');
+      expect(byteSize('')).toBe('0 bytes');
     });
   });
 });
-
--- src/util/data-util.ts
+++ src/util/data-util.ts
@@ -1,26 +1,12 @@
-/*
- * Copyright (c) 2021 Huy Duc Dao
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import * as React from 'react';
 
 export const openFile = (contentType: string, data: string) => () => {
   const fileURL = `data:${contentType};base64,${data}`;
   const win = window.open();
   win.document.write(
     '<iframe src="' +
-      fileURL +
-      '" frameborder="0" style="border:0; top:0; left:0; bottom:0; right:0; width:100%; height:100%;" allowfullscreen></iframe>'
+    fileURL +
+    '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
   );
 };
 
